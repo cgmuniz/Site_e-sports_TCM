a9d19aee2635ba495e039099d48a018313df677e
Cauã Gabriel <84600156+cgmuniz@users.noreply.github.com>
2023-05-19 00:28:19 -0300

    Site sobre e-sports
    TCM do 1º ano do ensino médio

---
+++ config.js
@@ -0,0 +1,7 @@
+function verClick(){
+	if (document.getElementById("links").style.display==="none"){
+		document.getElementById("links").style.display="block";
+	}else{
+		document.getElementById("links").style.display="none";
+	}
+}
